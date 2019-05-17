44bc9922d93574ec90f1d9a4f60551b0e44b703a
Nmeregini Vincent <nmereginivincent@gmail.com>
2019-05-17 11:26:00 +0100

    implemented strech goal carousel class

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,26 @@
 class Carousel {
+    constructor(domElement) {
+        this.domElement = domElement;
+        this.images = document.querySelectorAll('img');
+        this.btnRight = document.querySelector('.right-button');
+        this.btnLeft = document.querySelector('.left-button');
+        this.index = 1
+        this.btnRight.addEventListener('click', ()=>this.rightSlide())
+    }
 
+    rightSlide() {
+        this.hideSlide()
+        const imageArray =Array.from(this.images)
+        this.index < 1 ? this.index++ : this.index--
+        imageArray[this.index].classList.add('show-image')
+    }
+
+    hideSlide() {
+        this.images.forEach(image=> image.classList.remove('show-image'))
+    }
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelectorAll('img');
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
@@ -12,3 +30,4 @@ let carousel = document.querySelector();
     5. Think of how you would animate this compoennt. Make the cards slide in and out, or fade. It's up to you!
     6. Have fun!
 */
+new Carousel(carousel)
